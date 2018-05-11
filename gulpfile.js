100cacea2c7b272569884ce113014a83e3aa1fe8
Tali Marcus <tali.marcus@ge.com>
2018-05-11 11:58:28 -0700

    add gulp license task and missing bower imports

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ const bump = require('gulp-bump');
 const argv = require('yargs').argv;
 const cache = require('gulp-cached');
 const exec = require('child_process').exec;
+const { ensureLicense } = require('ensure-px-license');
 
 const sassOptions = {
   importer: importOnce,
@@ -71,6 +72,7 @@ gulp.task('sass', function() {
         return path.basename(file.path, path.extname(file.path)) + '-styles';
       }
     }))
+    .pipe(ensureLicense())
     .pipe(gulp.dest('css'))
     .pipe(browserSync.stream({match: 'css/*.html'}));
 });
@@ -121,6 +123,12 @@ gulp.task('bump:major', function(){
   .pipe(gulp.dest('./'));
 });
 
+gulp.task('license', function() {
+  return gulp.src(['./**/*.{html,js,css,scss}', '!./node_modules/**/*', '!./bower_components?(-1.x)/**/*'])
+    .pipe(ensureLicense())
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('default', function(callback) {
-  gulpSequence('clean', 'sass', 'generate-api')(callback);
+  gulpSequence('clean', 'sass', 'generate-api', 'license')(callback);
 });
